df87e72a0a01f570fe2b83308fe7d0de2e371113
Angie Kassab <anasab12@gmail.com>
2018-05-02 18:17:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,15 @@
 def fizzbuzz(int)
     if int % 3== 0
     puts "Fizz"
-  end
+
+  elsif int % 5== 0
+    puts "Buzz"
+    
+  else int % 3== 0 && int % 5== 0
+    puts "nil"
+end
 end
 
-fizzbuzz(3)
-fizzbuzz(5)
+# continue later if n % 3 == 0 *This is saying, if my number(n) is divisible by 3 leaving a 0 remainder.
 
+#                
