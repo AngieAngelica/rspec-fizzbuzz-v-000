6fb04afbf0288cfea15501afb4cd6d129f01a7ca
Angie Kassab <anasab12@gmail.com>
2018-05-02 18:26:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,11 +5,11 @@ def fizzbuzz(int)
     elsif int % 3== 0
      "Fizz"
 
-  elsif int % 5== 0
+    elsif int % 5== 0
      "Buzz"
     
   
-  else  int % 3== 0 || int % 5== 0
+    else  int % 3== 0 || int % 5== 0
      nil
 end
 end
